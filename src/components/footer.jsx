e788f8b4beb86636fa8f432bed26856d8fb347e9
Princekr19 <official.princekr@gmail.com>
2023-08-08 19:20:16 +0530

    initial commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer class="footer bg-dark py-3 mt-auto">
+      <div class="container text-center">
+        <span class="text-muted">
+          Made with Love by &copy;Prince Kumar{" "}
+          <a href="https://github.com/Princekr19">
+            <FaGithub />
+          </a>{" "}
+          <a href="https://www.linkedin.com/in/prince-kumar-501302217/">
+            <FaLinkedin />
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
